add2544bf773eb0a9f259922b3dfc517471f7c94
Sevde Ceylan <sevde.ceylan@student.uni-tuebingen.de>
2020-10-15 15:18:54 +0200

    used data from sagt

--- RegEx_3.0.py
+++ RegEx_3.0.py
@@ -2,14 +2,15 @@ import re
 
 output = open("output.txt","w+")
 
-with open('ger_lex.txt') as infile:
+with open('sagt_dict.txt') as infile:
     lexicon = infile.read()
     lexicon = lexicon.replace('\n', '|')
     #print(lexicon)
 
-corpus = open("text.txt", "r")
+corpus = open("sagt_corpus.txt", "r")
 text = corpus.read()
 corpus.close()
+
 
 nouns = r'('+lexicon+')'
 #print(nouns)
@@ -17,18 +18,35 @@ nouns = r'('+lexicon+')'
 #print(suffixes)
 #print(nouns+suffixes)
 
-mixed = re.compile(nouns+r'(?P<suffix>(ler|lar)?((i|ı|u|ü|)?m|(i|ı|u|ü)?n|si|sı|su|sü|i|ı|u|ü|imiz|ımız|umuz|ümüz|miz|mız|mus|müz|iniz|ınız|unuz|ünüz|niz|nız|nuz|nüz|leri|ları)?(yi|yı|yu|yü)?(ye|ya)?(den|dan|ten|tan)?(de|da|te|ta)?(nin|nın|nün|nun)?(in|ın|un|ün)?)')
+mixed = re.compile(nouns+r'(\')?(?P<suffix>(ler|lar)?((i|ı|u|ü|)?m|(i|ı|u|ü)?n|si|sı|su|sü|i|ı|u|ü|imiz|ımız|umuz|ümüz|miz|mız|mus|müz|iniz|ınız|unuz|ünüz|niz|nız|nuz|nüz|leri|ları)?(yi|yı|yu|yü|i|ı|u|ü)?(ye|ya|a|e)?(den|dan|ten|tan)?(de|da|te|ta)?(nin|nın|nün|nun)?(in|ın|un|ün)?)')
 #mixed =(nouns+suffixes)
 #print(mixed)
 #match = re.findall(mixed,text)
 #print (match)
 
+count = 0
+
 for match in re.finditer(mixed, text):
     if(match.group('suffix')):
+        count +=1
         output.write(match.group()+"\n")
         print(match.group())
 
+#expected amount of tokens
+corpus = open("sagt_corpus.txt", "r")
+line_count = 0.
+for line in corpus:
+    if line != "\n":
+        line_count += 1.
+corpus.close()
 
+print("")
+print("EXPECTED AMOUNT OF WORDS: ")
+print(line_count)
+print("AMOUNT OF FOUND MIXED WORDS: ")
+print(count)
+print("ACCURACY :")
+print(count/line_count)
 
 
 
